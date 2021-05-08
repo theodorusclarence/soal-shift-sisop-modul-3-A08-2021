d72659f8aef1057ad151754feb20b766b331548c
Clarence <theodorusclarence@gmail.com>
2021-05-08 15:09:33 +0700

    feat: add soal2c

---
+++ soal2/soal2c.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  pid_t child_id;
+  int status;
+  int fp1[2];
+  int fp2[2];
+  char ouput[1000];
+
+  if (pipe(fp1) == -1) {
+    fprintf(stderr, "Pipe Failed");
+    return 1;
+  }
+
+  if (pipe(fp2) == -1) {
+    fprintf(stderr, "Pipe Failed");
+    return 1;
+  }
+
+  child_id = fork();
+  if (child_id < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (child_id == 0) {
+    // nutup read
+    close(fp1[0]);
+
+    // duplicate write ke terminal
+    dup2(fp1[1], STDOUT_FILENO);
+
+    // exec ls
+    char *argv[] = {"ps", "aux", NULL};
+    execv("/bin/ps", argv);
+  } else {
+    // this is parent
+    while ((wait(&status)) > 0)
+      ;
+    // Close write pipe1
+    close(fp1[1]);
+
+    child_id = fork();
+    if (child_id < 0) {
+      exit(EXIT_FAILURE);
+    }
+    if (child_id == 0) {
+      // close write pipe1
+      close(fp1[1]);
+
+      // duplicate read
+      dup2(fp1[0], STDIN_FILENO);
+
+      // close read
+      close(fp2[0]);
+
+      // duplicate write ke termin
+      dup2(fp2[1], STDOUT_FILENO);
+
+      // exec
+      char *argv[] = {"sort", "-nrk", "3,3", NULL};
+      execv("/usr/bin/sort", argv);
+    } else {
+      while ((wait(&status)) > 0)
+        ;
+      // close write
+      close(fp2[1]);
+
+      // dup read ke stdin
+
+      dup2(fp2[0], STDIN_FILENO);
+
+      char *argv[] = {"head", "-5", NULL};
+      execv("/usr/bin/head", argv);
+    }
+  }
+}
